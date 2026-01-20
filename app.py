e3064ed1e43f6b250c4e39e0f0b2988fc15d959a
Sajjad Ali Noor <proactive1.san@gmail.com>
2026-01-20 10:24:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
+
 
 # ----------------------------
 # App Config
@@ -28,7 +29,7 @@ else:
     st.success("GEMINI_API_KEY loaded successfully.")
 
 
-st.title("AI Resume Matcher")
+st.title("AI Resume Matcher(10:24)")
 st.write("Upload a candidate CV to see which jobs fit best.")
 
 # ----------------------------
